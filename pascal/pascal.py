1afa7d3adab1629fbb34466f8fa3d4f8b980ff07
Dahaka1 <ijoech@gmail.com>
2022-11-04 16:46:06 +0300

    New Version

--- pascal/pascal.py
+++ pascal/pascal.py
@@ -1,3 +1,18 @@
-print('Hello3')
-print('Hello Yaroslav')
-print('test 1')
+n, m = int(input()), int(input())
+
+matrix = []
+
+for _ in range(n):
+    matrix.append(input().split())
+
+data = [k for k in input().split()]
+i, j = int(data[0]), int(data[1])
+
+for stroke in matrix:
+    stroke[i], stroke[j] = stroke[j], stroke[i]
+
+for stroke in matrix:
+    for elem in stroke:
+        print(elem, end=' ')
+    print()
+
