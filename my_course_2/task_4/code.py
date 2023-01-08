d78e336af7353f50e6a4ce201882fa4f6a989bab
Dahaka1 <ijoech@gmail.com>
2023-01-08 12:35:27 +0300

    new task

---
+++ my_course_2/task_4/code.py
@@ -0,0 +1,10 @@
+import csv
+
+def sorting(num):
+	with open('../deniro.csv', encoding='utf-8') as file_in:
+		lst = list(csv.reader(file_in))
+		return sorted([list(map(lambda x: int(x) if i.index(x) == num - 1 and str(x).isdigit() else str(x), i)) for i in lst], key=lambda j: j[num - 1])
+
+
+for j in sorting(int(input())):
+	print(*j, sep=',')
