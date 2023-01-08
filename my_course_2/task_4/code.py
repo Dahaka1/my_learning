52a9057e46e828a3da0864f795b45ff3946484ab
Dahaka1 <ijoech@gmail.com>
2023-01-08 12:36:11 +0300

    new task

--- my_course_2/task_4/code.py
+++ my_course_2/task_4/code.py
@@ -3,7 +3,7 @@ import csv
 def sorting(num):
 	with open('../deniro.csv', encoding='utf-8') as file_in:
 		lst = list(csv.reader(file_in))
-		return sorted([list(map(lambda x: int(x) if i.index(x) == num - 1 and str(x).isdigit() else str(x), i)) for i in lst], key=lambda j: j[num - 1])
+		return sorted([list(map(lambda x: int(x) if i.index(x) == num - 1 and str(x).isdigit() else str(x), i)) for i in lst], key=lambda k: k[num - 1])
 
 
 for j in sorting(int(input())):
